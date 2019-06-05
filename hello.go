2808d8100817d57f3ca0a90772a3cdbeb1a8594b
Michael Hrivnak <mhrivnak@redhat.com>
2019-06-05 18:01:57 +0200

    configurable greeting

--- hello.go
+++ hello.go
@@ -8,8 +8,12 @@ import (
 
 func main() {
 	hostname := os.Getenv("HOSTNAME")
+	greeting := os.Getenv("GREETING")
+	if greeting == "" {
+		greeting = "Hello"
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from %s!\n", hostname)
+		fmt.Fprintf(w, "%s from %s!\n", greeting, hostname)
 	})
 
 	http.ListenAndServe(":8080", nil)
